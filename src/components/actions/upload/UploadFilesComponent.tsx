a5d9cab95cad5a5c56ceec6d538284812471ec86
CoricovMA <coricovacMA@gmail.com>
2022-11-05 17:14:14 -0400

    added file upload, but no real styling, just basic stuff

--- src/components/actions/upload/UploadFilesComponent.tsx
+++ src/components/actions/upload/UploadFilesComponent.tsx
@@ -1,37 +1,50 @@
 import React, { useContext, useState } from 'react';
-import Dropzone from 'react-dropzone'
 import { AppContext, AppContextType } from '../../../context/AppContext'
+import { getStorage, uploadBytes, ref} from 'firebase/storage'
 
 export function UploadedFileComponent(){
-
+    return (<></>)
 }
 
 export function UploadFilesComponent(){
-    const { user } = useContext(AppContext) as AppContextType
     const [fileList, setFileList] = useState([])
+    const storage = getStorage()
     
-    const handleUploadFile = async(event) =>{
-         return true;
+    const handleUploadFile = async() =>{
+        fileList.forEach(item => 
+            uploadBytes(item.fileRef, item.file).then((snapshot) => 
+            {
+                console.log('Uploading file')
+            }
+        ))
     }
 
-    return (
-     <>
-     <Dropzone>
+    const handleFileDrop = (file) => { 
+        if(file.target.files[0] == undefined){
+            return;
+        }
+        const fileRef = ref(storage, 'customerdata/' + file.target.files[0].name)
+        fileList.push({
+            'file': file.target.files[0],
+            'fileRef': fileRef
+        })
+        setFileList(fileList)
+    }
 
-     </Dropzone>
-            {/* <div className="flex justify-center items-center w-full">
-                <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                    <div className="flex flex-col justify-center items-center pt-5 pb-6">
-                        <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                    </div>
-                    <input id="dropzone-file" type="file" accept='.png .jpg .jpeg .pdf .docx .excel' className="hidden" multiple />
-                </label>
-            </div> 
-            <div>
-                {fileList.map(item => item)}
-            </div> */}
+
+    return (
+      <>
+      <div>
+        <input type="file" accept='.png .jpg .jpeg .pdf .docx .excel' multiple onChange={handleFileDrop}/> 
+        <div>
+            <button 
+            onClick={handleUploadFile}
+            className='inline-flex w-full justify-center items-center rounded-full border border-transparent px-4 py-2 text-lg font-semibold text-white shadow-sm bg-gray-700 hover:bg-gray-500 sm:ml-3 sm:w-auto sm:text-sm'
+            >
+                Upload files
+            </button>
+        </div>
+      </div>
       </>
     )
 }
