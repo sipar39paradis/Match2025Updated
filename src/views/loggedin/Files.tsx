73af3fb23264861203d2a5149c8e639fa52d6df8
CoricovMA <coricovacMA@gmail.com>
2022-11-06 16:47:50 -0500

    refactored some stuff, added file svg. moved component layout. Styling left to be done

--- src/views/loggedin/Files.tsx
+++ src/views/loggedin/Files.tsx
@@ -1,48 +1,66 @@
 import '../../i18n/config'
 import { useTranslation } from 'react-i18next';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext, AppContextType } from '../../context/AppContext'
 import { getStorage, uploadBytes, ref, listAll, StorageReference, getDownloadURL, } from 'firebase/storage'
-import { arrayBuffer } from 'stream/consumers';
+import {ReactComponent as BlankFile} from '../../icons/BlankFile.svg'
+
 const FOLDER_NAME = 'customerdata/'
 
-interface FileUrl{
+interface FileInfo{
     url: string;
     fullPath: string;
 }
 
-export function FileDisplay(props: FileUrl){
+interface ReloadProps{
+    shouldReload: boolean;
+}
 
-    const isPicture = (itemRef: StorageReference) => {
-        return !(itemRef.fullPath.includes('.pdf'))
+export function FileComponent(props: FileInfo){
+    return (<div className='center'>
+        <BlankFile className='transition ease-in-out delay-150 hover:-translate-y-1 fill-blue-500 lg:w-1/2 mt-2 mb-2'/>
+    </div>
+    )
+
+}
+
+export function FilesDisplay(props: ReloadProps){
+    const { user } = useContext(AppContext) as AppContextType
+    const storage = getStorage()
+    const [files, setFiles] = useState([])
+    const [filesDisplayed, setFilesDisplayed] = useState(props.shouldReload)
+
+    const displayFiles = () => {
+        const folderRef = ref(storage, FOLDER_NAME + user.uid)
+        listAll(folderRef).then((res) => {
+            res.items.forEach(
+                (itemRef) => {
+                    getDownloadURL(itemRef).then((res) =>{
+                            setFiles( fileDisplay => [...fileDisplay,  <FileComponent url={res} fullPath={itemRef.fullPath} key={res}/>])
+                    })
+                }
+            )
+        })
     }
 
-    return (<>
-        <div className="p-2 rounded-lg shadow-xl lg:max-w-fit lg:min-w-fit peer">
-
-        {   (isPicture)
-            ?
-            <img
-                className="object-cover w-full lg:w-40 lg:h-40"
-                src={props.url}
-                alt="image"
-            />
-            :
-            <object
-                className="object-cover w-full lg:w-40 lg:h-40"
-                data={props.url}
-                type="application/pdf"
-            />
-        }
+    if(!filesDisplayed){
+        displayFiles()
+        setFilesDisplayed(true)
+    }
+    
+    return (
+    <>
+        <div className='grid grid-cols-5 gap-4 place-items-center'>
+            {files}
         </div>
-
-    </>)
+    </>
+    )
 }
 
 export function UploadFilesComponent(){
-    const { user } = useContext(AppContext) as AppContextType
     const [fileList, setFileList] = useState([])
-    const [fileDisplay, setFileDisplay] = useState([])
+    const [fileDisplay, setFileDisplay] = useState(<FilesDisplay shouldReload={false}/>)
+    const { user } = useContext(AppContext) as AppContextType
     const storage = getStorage()
     
 
@@ -53,8 +71,8 @@ export function UploadFilesComponent(){
                 console.log('Uploading file')
             }
         ))
-
-        displayFiles()
+        
+        setFileDisplay(<FilesDisplay shouldReload={false} />)
     }
 
     const handleFileDrop = (file) => { 
@@ -67,36 +85,24 @@ export function UploadFilesComponent(){
             'fileRef': fileRef
         })
         setFileList(fileList)
-
     }
 
-    const displayFiles = () => {
-        const folderRef = ref(storage, FOLDER_NAME + user.uid)
-        listAll(folderRef).then((res) => {
-            res.items.forEach(
-                (itemRef) => {
-                    getDownloadURL(itemRef).then((res) =>{
-                            setFileDisplay( fileDisplay => [...fileDisplay,  <FileDisplay url={res} fullPath={itemRef.fullPath} key={res}/>])
-                    })
-                }
-            )
-        })
-    }
+
 
     return (
       <>
       <div>
+        <div >
+            {fileDisplay}
+        </div>
         <input type="file" accept='.png .jpg .jpeg .pdf .docx .excel' multiple onChange={handleFileDrop}/> 
-        <div>
+        <div >
             <button 
             onClick={handleUploadFile}
             className='inline-flex w-full justify-center items-center rounded-full border border-transparent px-4 py-2 text-lg font-semibold text-white shadow-sm bg-gray-700 hover:bg-gray-500 sm:ml-3 sm:w-auto sm:text-sm'
             >
                 Upload files
             </button>
-        </div>
-        <div>
-            {fileDisplay}
         </div>
       </div>
       </>
