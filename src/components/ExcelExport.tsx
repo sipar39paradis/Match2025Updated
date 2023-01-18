c9511f14a92f5a188be61769a9681a05d07ca359
CoricovMA <coricovacMA@gmail.com>
2023-01-17 23:31:25 -0500

    set types for input'

--- src/components/ExcelExport.tsx
+++ src/components/ExcelExport.tsx
@@ -2,7 +2,9 @@ import * as FileSaver from 'file-saver'
 import XLSX from 'sheetjs-style'
 import React from 'react'
 
-export const exportToExcel = async (excelData, fileName) =>{
+// ecelData must be in the format [{}]
+// where the given object is an array of objects
+export const exportToExcel = async (excelData: any[], fileName: string) =>{
     const fileType = 'application/vnd.openxmlformats-officialdocument.spreadsheetml.sheet;charset=UTF-8'
     const fileExtension = '.xlsx';
 
